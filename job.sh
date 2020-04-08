5431dc4f30fe0de53414b7f027f11f6fcbc4a2d6
Tomas Hrnciar <thrnciar@redhat.com>
2020-04-08 23:03:30 +0200

    Evmapy, job: pass correct parameter for all months in year

--- job.sh
+++ job.sh
@@ -4,4 +4,19 @@ month=$(date +'%-m')
 year=$(date +'%Y')
 
 cd /home/ckan/Software/diplomka/
-python3 /home/ckan/Software/diplomka/evmapy.py -in -sy $year -sm $((month-1)) -ey $year -em $((month-1))
+if [ $(( $i  % 12 -1)) -eq "-1" ] # november
+then
+	python3 /home/ckan/Software/diplomka/evmapy.py -in -sy $year -sm 11 -ey $year -em 11
+	echo $(( $i  % 12 -1 ))
+	echo "import novembert"
+elif [ $(( $i  % 12 -1)) -eq "0" ] # december
+then
+	python3 /home/ckan/Software/diplomka/evmapy.py -in -sy $((year-1)) -sm 12 -ey $((year-1)) -em 12
+	echo $(( $i  % 12 -1 ))
+	echo "import december"
+else # rest of the year
+	python3 /home/ckan/Software/diplomka/evmapy.py -in -sy $year -sm $((month-1)) -ey $year -em $((month-1))
+	echo $(( $i  % 12 -1 ))
+	echo "import zvysok"
+fi
+
