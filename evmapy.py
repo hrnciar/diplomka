dbfa09ff656986d5817b2cf2510ea8ca7e77d99e
Tomas Hrnciar <thrnciar@redhat.com>
2020-04-17 21:18:45 +0200

    Evmapy: Adds rollback option, to recover if something fails

--- evmapy.py
+++ evmapy.py
@@ -7,15 +7,17 @@ import os
 import csv
 import argparse
 from datetime import datetime
+import logging
 import requests
 from bs4 import BeautifulSoup
-import ckanapi
 import toml
-import logging
+from shutil import copyfile
 
 config = toml.load('config.toml')
 
 EXIT_REQUEST_ERROR = 1
+EXIT_ROLLBACK_SUCCESS = 2
+EXIT_ROLLBACK_ERROR = 3
 
 def get_data(url, period, station, pump):
     """Scrape data from web"""
@@ -38,11 +40,11 @@ def get_data(url, period, station, pump):
             exit(EXIT_REQUEST_ERROR)
         except requests.exceptions.HTTPError as e:
             logging.error(e)
-            logging.error('Request for retrieving resource id failed. Exiting...')
+            logging.error('Request for retrieving table data failed. Exiting...')
             exit(EXIT_REQUEST_ERROR)
         except requests.exceptions.RequestException as e:
             logging.error(e)
-            logging.error('Request for retrieving resource id failed. Exiting...')
+            logging.error('Request for retrieving table data failed. Exiting...')
             exit(EXIT_REQUEST_ERROR)
 
         return request
@@ -153,13 +155,50 @@ def ckan_post_request(url, action, data, headers, filename):
         r.raise_for_status()
     except requests.exceptions.HTTPError as e:
         logging.error(e)
-        logging.error('Request for retrieving resource id failed. Exiting...')
+        logging.error('Request for action: %s failed. Exiting...', action)
         return EXIT_REQUEST_ERROR
     except requests.exceptions.RequestException as e:
-        logging.error('Request for retrieving resource id failed. Exiting...')
+        logging.error('Request for action: %s failed. Exiting...', action)
         return EXIT_REQUEST_ERROR
 
     return 0
+
+def rollback(start_year, end_year):
+    rollback_error = False
+    for year in range(start_year, end_year+1):
+        logging.error('Rollback of year %s in progress', year)
+        filename = config['filename'] + str(year) + config['extension']
+
+        # Remove corrupted files
+        try:
+            os.remove(filename)
+        except:
+            pass
+        # Restore backed up files
+        copyfile(filename + '.old', filename)
+
+        # Remove old back ups
+        os.remove(filename + ".old")
+
+        extension = os.path.splitext(filename)[1][1:].upper()
+        logging.info('Creating "{resource_name}" resource'.format(**locals()))
+        data = {
+            'package_id': config['package'],
+            'name': year,
+            'format': extension,
+            'url': 'upload',  # Needed to pass validation
+        }
+        headers = {'Authorization': config['apikey']}
+        r = ckan_post_request(config['url_api'], 'resource_create', data, headers, filename)
+
+        if r == EXIT_REQUEST_ERROR:
+            rollback_error = True
+            logging.critical('FATAL ERROR: Rollback for year %s failed, trying to rollback the rest', year)
+
+    if rollback_error:
+        return EXIT_ROLLBACK_ERROR
+
+    return EXIT_ROLLBACK_SUCCESS
 
 parser = argparse.ArgumentParser(description='Import Evmapy data to CKAN')
 
@@ -181,13 +220,13 @@ args = parser.parse_args()
 logging.basicConfig(filename='evmapy.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 if ((len(str(args.start_year)) != 4) or (len(str(args.end_year)) != 4)):
-        logging.error('Given year does not has 4 digits. Exiting...')
-        exit(1)
+    logging.error('Given year does not has 4 digits. Exiting...')
+    exit(1)
 
 if (((len(str(args.start_month)) >= 2) and (len(str(args.start_year)) <= 0)) or
-    ((len(str(args.end_month)) >= 2) and (len(str(args.end_year)) <= 0))):
-        logging.error('Given month does not has 2 digits. Exiting...')
-        exit(1)
+        ((len(str(args.end_month)) >= 2) and (len(str(args.end_year)) <= 0))):
+    logging.error('Given month does not has 2 digits. Exiting...')
+    exit(1)
 
 logging.debug('Arguments parsed.')
 if args.head:
@@ -195,8 +234,19 @@ if args.head:
 if args.no_head:
     head_written = True
 
+for year in range(args.start_year, args.end_year+1):
+    filename = config['filename'] + str(year) + config['extension']
+
+    # Create backup of file being uploaded
+    copyfile(filename, filename + '.old')
+    logging.info('Backing up %s', filename)
+
+    if args.import_old:
+        os.remove(filename)
+        logging.info('Removed %s', filename)
+
 for data, y, m, counter in month_year_iter(args.start_month, args.start_year, args.end_month, args.end_year):
-    filename = config['filename'] + str(y) + config['extension'] # evmapy_xxxx.csv
+    filename = config['filename'] + str(y) + config['extension'] # evmapy-backup/evmapy_xxxx.csv
 
     if os.path.exists(filename):
         append_write = 'a' # append if already exists
@@ -209,6 +259,7 @@ for data, y, m, counter in month_year_iter(args.start_month, args.start_year, ar
         exit(1)
     logging.debug('File opened')
     writer = csv.writer(outfile)
+
     if data != 'Err - empty table':
         if not head_written:
             writer.writerow(config['table_head'])
@@ -220,8 +271,14 @@ for data, y, m, counter in month_year_iter(args.start_month, args.start_year, ar
         outfile.close()
     if args.import_old:
         logging.info('Importing old datas')
+
+        if y != args.end_year:
+            months_in_year = 12
+        else:
+            months_in_year = args.end_month
+
         # Ending loop after end_month iterations and all sockets proccessed
-        if m == args.end_month and counter == len(config['socket_dict']):
+        if m == months_in_year and counter == len(config['socket_dict']):
             head_written = False
 
             path = os.path.join(filename)
@@ -237,8 +294,8 @@ for data, y, m, counter in month_year_iter(args.start_month, args.start_year, ar
             headers = {'Authorization': config['apikey']}
             r = ckan_post_request(config['url_api'], 'resource_create', data, headers, filename)
             if r == EXIT_REQUEST_ERROR:
-                # TODO: call rollback() here
-                exit(1)
+                exit(rollback(args.start_year, args.end_year))
+
     if args.import_new:
         try:
             # TODO: use ckan_post_request() instead
@@ -250,12 +307,10 @@ for data, y, m, counter in month_year_iter(args.start_month, args.start_year, ar
         except requests.exceptions.HTTPError as e:
             logging.error(e)
             logging.error('Request for retrieving resource id failed. Exiting...')
-            # TODO: call rollback() here
-            raise SystemExit(e)
+            exit(rollback(args.start_year, args.end_year))
         except requests.exceptions.RequestException as e:
             logging.error('Request for retrieving resource id failed. Exiting...')
-            # TODO: call rollback() here
-            raise SystemExit(e)
+            exit(rollback(args.start_year, args.end_year))
         data = r.json()
         resource_id = ''
         for resource in data['result']['resources']:
@@ -277,8 +332,7 @@ for data, y, m, counter in month_year_iter(args.start_month, args.start_year, ar
             headers = {'Authorization': config['apikey']}
             r = ckan_post_request(config['url_api'], 'resource_create', data, headers, filename)
             if r == EXIT_REQUEST_ERROR:
-                # TODO: call rollback() here
-                exit(1)
+                exit(rollback(args.start_year, args.end_year))
         else:
             logging.info('Updating "{resource_name}" resource'.format(**locals()))
             data = {
@@ -291,8 +345,13 @@ for data, y, m, counter in month_year_iter(args.start_month, args.start_year, ar
             headers = {'Authorization': config['apikey']}
             r = ckan_post_request(config['url_api'], 'resource_update', data, headers, filename)
             if r == EXIT_REQUEST_ERROR:
-                # TODO: call rollback() here
-                exit(1)
+                exit(rollback(args.start_year, args.end_year))
 
     logging.info('All datas successfully imported.')
 
+for year in range(args.start_year, args.end_year+1):
+    filename = config['filename'] + str(year) + config['extension']
+
+    # Remove old backup, keep new one
+    os.remove(filename + '.old')
+    logging.info('Removing old backups %s', filename)
